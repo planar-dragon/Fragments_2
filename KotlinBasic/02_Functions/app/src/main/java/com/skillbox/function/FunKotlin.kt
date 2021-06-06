0ce81303ae42484faa1714df1a25ee13be1494fb
Rakhmatullin Rinat <anclod@mail.ru>
2021-06-06 17:47:18 +0500

    function

--- KotlinBasic/02_Functions/app/src/main/java/com/skillbox/function/FunKotlin.kt
+++ KotlinBasic/02_Functions/app/src/main/java/com/skillbox/function/FunKotlin.kt
@@ -3,7 +3,7 @@ import kotlin.math.sqrt
 
 fun main() {
 
-    val solutionSum = solveEquation(a = 52, b = -505, c = 425)
+    val solutionSum = solveEquation(a = 52, b = -5005, c = 425)
     println(solutionSum)
 }
 fun square(b: Double) = b * b
@@ -13,16 +13,19 @@ fun solveEquation(a: Int, b: Int, c: Int): Double {
     val discriminant = square(b.toDouble()) - (4 * a * c)
 
     // Если D>0, то уравнение имеет два корня, которые находятся по формулам:
-    val x1 = ((-b) + sqrt(discriminant)) / (2 * a)
-    val x2 = ((-b) - sqrt(discriminant)) / (2 * a)
-
-    // Если D<0, то уравнение не имеет корней.
-    if (discriminant < 0) {
-    println("Уравнение не имеет корней")
-    } else {
+    if (discriminant > 0) {
+        val x1 = ((-b) + sqrt(discriminant)) / (2 * a)
+        val x2 = ((-b) - sqrt(discriminant)) / (2 * a)
     println(x1)
     println(x2)
-    }
+
     //сумму корней
-    return (x1 + x2)
-}
+    return x1 + x2
+
+    // Если D<0, то уравнение не имеет корней.
+    } else {
+    println("Уравнение не имеет корней")
+    }
+
+    return discriminant
+    }
