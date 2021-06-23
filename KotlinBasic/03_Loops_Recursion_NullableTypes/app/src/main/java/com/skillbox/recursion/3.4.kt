6edf7e79e4b31e0b25b27e7e0112af276d7f129b
Rakhmatullin Rinat <anclod@mail.ru>
2021-06-23 16:16:09 +0500

    3.4

--- KotlinBasic/03_Loops_Recursion_NullableTypes/app/src/main/java/com/skillbox/recursion/3.4.kt
+++ KotlinBasic/03_Loops_Recursion_NullableTypes/app/src/main/java/com/skillbox/recursion/3.4.kt
@@ -2,26 +2,22 @@ package com.skillbox.recursion
 
 fun main() {
     print("Сколько N чисел вы хотите ввести: ")
-    val n = readLine()?.toIntOrNull()
-    if (n != null) {
-        firstNumbers(n)
-    } else {
-        println("Вы ввели не число!")
-           }
-    }
+    val n = readLine()?.toIntOrNull() ?: return
 
-fun firstNumbers(n: Int) {
     var u = 1
     var positiveNumbers = 0 //Положительные числа
     var sumEntNumbers = 0 //Сумма введеных чисел
 
     while (u <= n) {
         print("Введите число №$u: ")
-        val secondNumbers: Int = readLine()?.toIntOrNull() ?: continue
-        u++
-        if (secondNumbers > 0)
-        positiveNumbers++
-        sumEntNumbers += secondNumbers
+        val Numbers: Int? = readLine()?.toIntOrNull()
+
+        if (Numbers != null && Numbers > 0) {
+            u++
+            positiveNumbers++
+            sumEntNumbers += Numbers
+
+        } else println("Вы ввели не отрицательное число или не число!")
     }
     println("Количество положительных чисел = $positiveNumbers")
     println("Сумма всех введенных чисел = $sumEntNumbers")
