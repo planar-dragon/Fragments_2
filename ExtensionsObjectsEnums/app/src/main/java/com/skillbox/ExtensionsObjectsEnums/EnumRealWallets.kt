ce1ad2a9320c8c32b727394c09617d6a37630bc8
Rakhmatullin Rinat <anclod@mail.ru>
2021-10-06 11:21:15 +0500

    ExtensionsObjectsEnums

---
+++ ExtensionsObjectsEnums/app/src/main/java/com/skillbox/ExtensionsObjectsEnums/EnumRealWallets.kt
@@ -0,0 +1,13 @@
+package com.skillbox.ExtensionsObjectsEnums
+
+enum class EnumRealWallets (
+    val numberOfBills: Int // свойство класса - колличество купюр
+    ) {
+    ONE (0), // номинал и колличество купюр этого номинала
+    FIVE (0),
+    TEN (0),
+    FIFTY (0),
+    HUNDRED (0);
+
+    }
+// Не смог понять как с Enum классом делать
--- ExtensionsObjectsEnums/app/src/main/java/com/skillbox/ExtensionsObjectsEnums/Wallets.kt
+++ ExtensionsObjectsEnums/app/src/main/java/com/skillbox/ExtensionsObjectsEnums/Wallets.kt
@@ -16,7 +16,7 @@ sealed class Wallets() {
 //    (дальше мы напишем функцию для обработки такого функционала).
 //    наследник класса Wallets - класс VirtualWallets (виртуальный кошелек)
 
-    class VirtualWallets : Wallets() {
+    class VirtualWallets(override var moneyAdded: Double) : Wallets() {
         private var amountOfMoneyRubel: Double = 0.0
         private var amountOfMoneyDollar: Double = 0.0
         private var amountOfMoneyEuro: Double = 0.0
@@ -33,50 +33,122 @@ sealed class Wallets() {
             }
         }
 
-//    Тут я застрял >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 //    Реализуйте метод moneyInUSD для каждого из наследников.
 
-        override fun moneyInUSD(): Double {
-            return when (addMoneyToVirtualWallet(currency, moneyAdded)){
-                Currency.RUBEL -> amountOfMoneyRubel * Currency.conversionToDollar.rubelToUSD
-                Currency.DOLLAR -> amountOfMoneyRubel * Currency.conversionToDollar.rubelToUSD
-                Currency.EURO -> amountOfMoneyRubel * Currency.conversionToDollar.rubelToUSD
-                else -> 0.0
-                            }
+        override fun moneyInUSD() {
+            Currency.RUBEL to amountOfMoneyRubel * Currency.conversionToDollar.rubelToUSD
+            Currency.DOLLAR to amountOfMoneyDollar * Currency.conversionToDollar.rubelToUSD
+            Currency.EURO to amountOfMoneyEuro * Currency.conversionToDollar.rubelToUSD;
+
         }
     }
 //    6.2. Реальный кошелёк хранит каждую валюту в виде приватного свойства MutableMap<Int, Int>,
 //    где в качестве ключа выступает номинал купюры, а в качестве значения — количество купюр.
 
-        class RealWallets : Wallets() {
+        class RealWallets(override var moneyAdded: Double) : Wallets() {
 //    реальный кошелек только целые числа, поэтому Int
-            private var amountOfMoneyRubel: Int = 0
-            private var amountOfMoneyDollar: Int = 0
-            private var amountOfMoneyEuro: Int = 0
 
-            private val realWallets = mutableMapOf<Int, Int>(
-                1 to 0,
-                5 to 0,
-                10 to 0,
-                50 to 0,
-                100 to 0
+        private var currencyRubel: Int = 0 // тип валюты
+        private var currencyDollar: Int = 0
+        private var currencyEuro: Int = 0
 
-            )
+        val realWallet: MutableMap<Int, Int> = mutableMapOf(
+          1 to 20, // номинал и колличество купюр этого номинала
+          5 to 10,
+          10 to 7,
+          50 to 3,
+          100 to 2
+        )
+
 //     7.2. Для реального кошелька понадобится три аргумента: тип валюты, номинал купюры, количество купюр
 
-            fun addMoneyToRealWallets(currency: Currency, realWallets: MutableMap<Int, Int>) {
-                return when (currency) {
-                    Currency.RUBEL -> realWallets.values += amountOfMoneyRubel
-                    Currency.DOLLAR -> realWallets.values += amountOfMoneyDollar
-                    Currency.EURO -> realWallets.values += amountOfMoneyEuro
-                }
+        fun addMoneyToRealWallets(currency: Currency, nominalOfBills: Int, numberOfBills: Double) {
+            var moneyInUSD = 0.0
+            var nominalOfBills = realWallet.keys
+            var numberOfBills = realWallet.values
+
+            Currency.RUBEL ->
+                 nominalOfBills * numberOfBills * Currency.conversionToDollar.rubelToUSD,
+
+            if (this = currency.RUBEL) moneyInUSD = nominalOfBills * numberOfBills * Currency.conversionToDollar.rubelToUSD,
+            if (this = currency.RUBEL) moneyInUSD = nominalOfBills * numberOfBills * Currency.conversionToDollar.rubelToUSD,
+            return moneyInUSD
+        }
+            for ((nominalOfBills,numberOfBills) in realWallet){
+                print (it)
+
+            }            }
+
+
+
+
+//        {
+//            ключ тип валюты: {
+//            ключ номинал купюры: число - количество купюр
+//        }
+//        }
+        fun sumOfTypeCurrency(
+            currencyRubel: MutableMap<String, String>,
+            realWallet.keys: = numberOfBills) {
+} ->
+
+        fun addMoneyToRealWallets() {
+            Currency.RUBEL to amountOfMoneyRubel += realWallets.values
+            Currency.DOLLAR to realWallets.values += amountOfMoneyDollar
+            Currency.EURO to realWallets.values += amountOfMoneyEuro
             }
 
-        override fun moneyInUSD() {
-            TODO("Not yet implemented")
+        val numberOfBills = $realWallet.keys * $realWallet.size
+
+
+
+
+
+//        for (userNumber in mutableList)
+//            println(${realRubelWallet.key} * S{realRubelWallet.value})
+
+//        fun addMoneyToRealWallets() {
+//        val One = EnumRealWallets.ONE.numberOfBills
+//        val Five = EnumRealWallets.FIVE.numberOfBills
+//        val Ten = EnumRealWallets.TEN.numberOfBills
+//        val Fifty = EnumRealWallets.FIFTY.numberOfBills
+//        val Hundred = EnumRealWallets.HUNDRED.numberOfBills
+//
+//
+////    for (nominalMoney in mutableListOfBills) {
+////                EnumRealWallets.ONE: 20,
+////                EnumRealWallets.FIVE: 10
+////                EnumRealWallets.TEN: 7
+////                EnumRealWallets.FIFTY: 3
+////                EnumRealWallets.HUNDRED: 2
+////
+////        fun amountOfMoneyRubel(): Float {
+////            return EnumRealWallets.ONE * EnumRealWallets.ONE.numberOfBills
+////        }
+////        private var typeOfMoneyRubel: Int = 0 // тип валюты
+////        private var typeOfMoneyDollar: Int = 0
+////        private var typeOfMoneyEuro: Int = 0
+//
+//
+//            }
+
+            override fun moneyInUSD() {
+                TODO("Not yet implemented")
             }
+
         }
 
 
-}
+
+
+
+
+
+
+
+
+
+
+
+
 
