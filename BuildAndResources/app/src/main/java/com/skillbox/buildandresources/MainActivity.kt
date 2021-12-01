e65b0429e52505bd84b8a83ff7dc02087a622b90
Rakhmatullin Rinat <anclod@mail.ru>
2021-12-02 00:01:55 +0500

    9.8 Домашняя работа

--- BuildAndResources/app/src/main/java/com/skillbox/buildandresources/MainActivity.kt
+++ BuildAndResources/app/src/main/java/com/skillbox/buildandresources/MainActivity.kt
@@ -1,20 +1,33 @@
 package com.skillbox.buildandresources
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
 // Переменная визуализации экрана. findViewById - поиск нужного текста по id в ресурсах (R.id.textView)
         val textView = findViewById<TextView>(R.id.textView)
-// Заполнение найденной области на экране
+
+        val count = 1
+        val pluralString = resources.getQuantityString(R.plurals.main_quantity_string, count, count)
+// доступ к ресурсу, для получения множественной строки с идентификатором main_quantity_string
+// 2 значение - строка которая соответствует переменной count
+// 3 значение - строка значение которое будет вставленно в слот отображения значения
+// Устанавливаем в параметр текста переменнцю с изменяемой строкой
+        textView.text = pluralString
+
+// Заполнение найденной области на экране, редыдущее использование textView.text перекроется
         textView.text = """
         Build type = ${BuildConfig.BUILD_TYPE}
         Flavor = ${BuildConfig.FLAVOR}
-        
+        VersionCode =${BuildConfig.VERSION_CODE}
+        VersionName =${BuildConfig.VERSION_NAME}
         """
     }
 }
