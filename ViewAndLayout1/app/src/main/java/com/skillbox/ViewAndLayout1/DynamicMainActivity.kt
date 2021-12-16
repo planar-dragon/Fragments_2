37b9fddba7e64f8a2026c7147f76a11f8d62809e
Rakhmatullin Rinat <anclod@mail.ru>
2021-12-16 16:48:13 +0500

    Merge remote-tracking branch 'origin/master'

---
+++ ViewAndLayout1/app/src/main/java/com/skillbox/ViewAndLayout1/DynamicMainActivity.kt
@@ -0,0 +1,22 @@
+package com.skillbox.ViewAndLayout1
+
+import android.os.Bundle
+import android.os.Handler
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_dynamic.*
+import kotlinx.android.synthetic.main.activity_main.*
+
+class DynamicMainActivity: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dynamic)
+
+
+
+
+    }
+
+
+}
+
--- ViewAndLayout1/app/src/main/java/com/skillbox/ViewAndLayout1/MainActivity.kt
+++ ViewAndLayout1/app/src/main/java/com/skillbox/ViewAndLayout1/MainActivity.kt
@@ -5,14 +5,19 @@ import android.os.Handler
 import android.text.Editable
 import android.text.TextWatcher
 import android.view.View
+import android.widget.LinearLayout
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.skillbox.ViewAndLayout1.databinding.ActivityDynamicBinding
 import com.skillbox.ViewAndLayout1.databinding.ActivityMainBinding
+import kotlinx.android.synthetic.main.activity_dynamic.*
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     // чтоб не использовать поиск по id, создаем переменную которая включает в себя все ссылки из activity_main.xml
     // это переменная включающая в себя класс Разметки
     lateinit var bindingClass: ActivityMainBinding
+    lateinit var bindingClassDinamic: ActivityDynamicBinding
 
     // переназначение функции отрисовки экрана
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,56 +26,72 @@ class MainActivity : AppCompatActivity() {
         bindingClass = ActivityMainBinding.inflate(layoutInflater)
         // Далее вводим ресурс из которого будут браться элементы отрисовки экрана
         setContentView(bindingClass.root)
+        bindingClassDinamic = ActivityDynamicBinding.inflate(layoutInflater)
+        setContentView(bindingClassDinamic.root)
 
+        val viewProgressBar =
+            // inflate - создает view из ресурса разметки (ресурс, контейнер разметки, автоматический вывод)
+            layoutInflater.inflate(R.layout.activity_dynamic, bindingClassDinamic.containerProgressBar, true)
+                // Вью ProgressBar запустится после нажатия кнопки "Вход"
+        viewProgressBar.apply {
+            bindingClass.loginButton.setOnClickListener {
+                addProgressBar()
+                LinearLayout.LayoutParams(
+                    LinearLayout.LayoutParams.WRAP_CONTENT,
+                    LinearLayout.LayoutParams.WRAP_CONTENT
+                )
+            }
+        }
+        bindingClassDinamic.containerProgressBar.addView(viewProgressBar)
 
-        // создаем обьект следящий за заполнением текста в полях
-//        val loginEmailPassword = object : TextWatcher {
-//            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
-//
-//            override fun afterTextChanged(p0: Editable?) {}
-//
-//            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
-////                    if (textEmail.isNotEmpty() && textPass.isNotEmpty() && checkBox.isChecked)
-////                        buttonLogin.isEnabled = true
-//            }
+// Слушатели ввода логина и пароля
 
+        bindingClass.textEmailAddress.addTextChangedListener(object : TextWatcher {
 
- // создаем слушателя для ввода текста, который запускаест функцию проверки заполнения полей
-
-        val loginEmailPassword = object : TextWatcher {
             override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
 
             override fun afterTextChanged(p0: Editable?) {}
 
-            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
-                verificationEmailPasswordChekbox()
-            }
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+        })
+
+        bindingClass.textPassword.addTextChangedListener(object : TextWatcher {
+
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+
+            override fun afterTextChanged(p0: Editable?) {}
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+        })
+
+// Cлушатель Checkbox, запускающий функцию проверку заполнения полей логина и пароля.
+
+        bindingClass.chekboxExemple.setOnClickListener {
+            verificationEmailPasswordChekbox()
         }
 
-        bindingClass.chekboxExemple.setOnCheckedChangeListener { checkboxView, check ->
-        }
+// Cлушатель кнопки "Вход", запускающий функцию login.
 
         bindingClass.loginButton.setOnClickListener {
             login()
         }
 
     }
-// функция проверки полей, если поля и чекбокс активны, включается кнопка
-    fun verificationEmailPasswordChekbox() {
 
-        bindingClass.loginButton.isEnabled = false
+// Функция проверки заполнения полей логина и пароля.
 
+    private fun verificationEmailPasswordChekbox() {
         if (bindingClass.textEmailAddress.toString()
-                .isNotEmpty() && bindingClass.textPassword.toString()
-                .isNotEmpty() && bindingClass.chekboxExemple.isChecked
+                .isNotEmpty() && bindingClass.textPassword.toString().isNotEmpty()
         )
             bindingClass.loginButton.isEnabled = true
-            login()
+        else bindingClass.loginButton.isEnabled = false
     }
+
+// Функция запуска ProgressBar, кнопка "Вход", поля логин и пароль, Checkbox деактивируются.
 
     private fun login() {
         bindingClass.loginButton.isEnabled = false
-        bindingClass.operationProgress.visibility = View.VISIBLE
         bindingClass.textEmailAddress.isEnabled = false
         bindingClass.textPassword.isEnabled = false
         bindingClass.chekboxExemple.isEnabled = false
@@ -82,16 +103,21 @@ class MainActivity : AppCompatActivity() {
 
         Handler().postDelayed({
             bindingClass.loginButton.isEnabled = true
-            bindingClass.operationProgress.visibility = View.INVISIBLE
             bindingClass.textEmailAddress.isEnabled = true
             bindingClass.textPassword.isEnabled = true
             bindingClass.chekboxExemple.isEnabled = true
 
+
             Toast.makeText(this, R.string.toast_login, Toast.LENGTH_SHORT).show()
         }, 2000)
     }
-// При нажатии кнопки должна деактивироваться кнопка,
+    fun addProgressBar() {
+        bindingClassDinamic.operationProgress.visibility = View.VISIBLE
+
+        Handler().postDelayed({ operationProgress.visibility = View.INVISIBLE }, 2000)
     }
+}
+
 
 
 
