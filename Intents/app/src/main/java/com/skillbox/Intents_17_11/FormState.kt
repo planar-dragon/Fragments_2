dfa4e20ae887a79603ca19750116cbc918a03fac
Rakhmatullin Rinat <anclod@mail.ru>
2022-01-27 15:52:54 +0500

    Add lesson

---
+++ Intents/app/src/main/java/com/skillbox/Intents_17_11/FormState.kt
@@ -0,0 +1,12 @@
+package com.skillbox.Intents_17_11
+
+
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+// Для того, чтобы хранить состояние формы, создайте класс.
+
+@Parcelize
+
+data class FormState(val valid: Boolean, val message: String) : Parcelable
+
